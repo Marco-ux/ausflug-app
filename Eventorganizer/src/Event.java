8d21246daad93d16c725c4a9c4a19c1a82a8dd71
marco <marco.gercken@plansfer.de>
2016-09-19 22:04:34 +0200

    Evtentclass and Participant created

---
+++ Eventorganizer/src/Event.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public abstract class Event {
+	
+	private String name;
+	private List<Participant> participants;
+	
+	public Event(String name, List<Participant> participants) {
+		super();
+		this.participants = new ArrayList<Participant>();
+		this.name = name;
+		this.participants = participants;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Participant> getParticipants() {
+		return participants;
+	}
+	public void setParticipants(List<Participant> participants) {
+		this.participants = participants;
+	}
+	
+	
+	
+}
